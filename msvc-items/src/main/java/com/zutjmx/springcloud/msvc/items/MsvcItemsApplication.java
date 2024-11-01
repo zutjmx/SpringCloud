594bce87a80418912604658623639cd5a3f9176c
Jesus Zuñiga Trejo <zutjmx@gmail.com>
2024-11-01 12:51:23 -0600

    Se modifica el nombre del paquete base de msvc-items

--- msvc-items/src/main/java/com/zutjmx/springcloud/msvc/items/MsvcItemsApplication.java
+++ msvc-items/src/main/java/com/zutjmx/springcloud/msvc/items/MsvcItemsApplication.java
@@ -1,4 +1,4 @@
-package com.zutjmx.springcloud.msvc.items.msvc_items;
+package com.zutjmx.springcloud.msvc.items;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
