7d2d60e40866ff3bcecc44c1f4fff5b1b19d04a8
Jesus Zuñiga Trejo <zutjmx@gmail.com>
2024-11-01 12:45:50 -0600

    Se agrega el proyecto msvc-items

---
+++ msvc-items/src/main/java/com/zutjmx/springcloud/msvc/items/msvc_items/MsvcItemsApplication.java
@@ -0,0 +1,13 @@
+package com.zutjmx.springcloud.msvc.items.msvc_items;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsvcItemsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsvcItemsApplication.class, args);
+	}
+
+}
---
+++ msvc-items/src/test/java/com/zutjmx/springcloud/msvc/items/msvc_items/MsvcItemsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zutjmx.springcloud.msvc.items.msvc_items;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsvcItemsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
