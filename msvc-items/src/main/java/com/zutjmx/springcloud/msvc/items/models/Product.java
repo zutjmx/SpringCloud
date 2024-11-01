074488fb76cf70d42b34289f01ade3685a607a8b
Jesus Zuñiga Trejo <zutjmx@gmail.com>
2024-11-01 13:11:20 -0600

    Se agrega Item, Product e ItemService

---
+++ msvc-items/src/main/java/com/zutjmx/springcloud/msvc/items/models/Product.java
@@ -0,0 +1,47 @@
+package com.zutjmx.springcloud.msvc.items.models;
+
+import java.time.LocalDate;
+
+public class Product {
+
+    private Long id;
+
+    private String name;
+
+    private Double price;
+
+    private LocalDate createAt;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public LocalDate getCreateAt() {
+        return createAt;
+    }
+
+    public void setCreateAt(LocalDate createAt) {
+        this.createAt = createAt;
+    }
+    
+}
